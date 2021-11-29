2c4082a51e2a519de20863032e0de6f95b01dba4
Connor Graham <connorgraham33@gmail.com>
2021-11-28 19:28:56 -0500

    Update main.py

--- main.py
+++ main.py
@@ -462,7 +462,7 @@ def RunIR():
         st.insert('end', dealermsg)
         all_actions = []
         for i in range(len(player_hands)):
-            if player == None or len(player.hands) < 1:
+            if player == None or len(player.hands) < 1 or player_hands == []:
                 continue
             if len(player_hands) > 1 and dealer_upcard != '':
                 action = table.get_player_action(player, i)
